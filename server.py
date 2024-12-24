c9aa9e4de4fd0c07642f6c2f7303d8382f6d6e29
Kenneth Philp <kennyphilp@Kenneths-Mac-mini-7.local>
2024-12-24 17:44:42 +0000

    Update

--- server.py
+++ server.py
@@ -12,8 +12,9 @@ def home():
     currentDate=dt.datetime.today()
 
     formattedDate= currentDate.strftime("%x")
+    formattedTime = dt.datetime.now().strftime("%H:%M:%S")
     print("The date is " + formattedDate)
-    return render_template("index.html", displayDate = formattedDate)
+    return render_template("index.html", displayDate = formattedDate, displayTime=formattedTime)
 
 if __name__=="__main__":
     app.run(debug=True)
